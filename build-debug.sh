bfc631986cf2f48f6ab2474be43cd3c71ef4e875
baltic <baltic@localhost>
2019-06-11 17:03:00 +0300

    -bazel leftovers

--- build-debug.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#bazel clean --expunge
-export CC="gcc-8"
-bazel build --cxxopt='-std=c++17' --cxxopt='-mavx2' archivarius --verbose_failures --compilation_mode dbg
-#export CC="clang-7"
-#bazel build --cxxopt=-std=c++17 --cxxopt='-mavx2' archivarius --verbose_failures --compilation_mode dbg --linkopt=-stdlib=libc++
-
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-export CC="gcc-8"
-bazel build --cxxopt='-std=c++17' --cxxopt='-mavx2' archivarius --verbose_failures --compilation_mode opt
