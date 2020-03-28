3850ee113d5795f4ec7c5bba2a4cc3655801247e
ds <none>
2020-03-28 22:17:12 +0300

    renames

--- console-tool.cmake
+++ console-tool.cmake
@@ -1,16 +1,18 @@
 include(lib.cmake)
 
+configure_file(${PROJECT_SOURCE_DIR}/version.h ${PROJECT_SOURCE_DIR}/archivarius/cli/)
+
 set(archivarius-tool-srcs 
-./archivarius/tool/cmd_line_parser.c++
-./archivarius/tool/cmd_line_parser.h
-./archivarius/tool/config.c++
-./archivarius/tool/config.h
-./archivarius/tool/main.c++
-./archivarius/tool/property_tree.c++
-./archivarius/tool/property_tree.h
-./archivarius/tool/test.c++
-./archivarius/tool/test.h
-./archivarius/tool/version.h
+./archivarius/cli/cmd_line_parser.c++
+./archivarius/cli/cmd_line_parser.h
+./archivarius/cli/config.c++
+./archivarius/cli/config.h
+./archivarius/cli/main.c++
+./archivarius/cli/property_tree.c++
+./archivarius/cli/property_tree.h
+./archivarius/cli/test.c++
+./archivarius/cli/test.h
+./archivarius/cli/version.h
 )
 
 
